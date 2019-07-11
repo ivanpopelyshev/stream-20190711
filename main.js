dd5836f0ea998c9c5ef53dcdc0b724a7daa164e9
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2019-07-11 23:40:04 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,437 @@
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+
+var screen = {
+    width: 40 * 18,
+    height: 41 * 18,
+    zoom: 1,
+    scale: 18
+};
+
+canvas.width = screen.width;
+canvas.height = screen.height;
+
+canvas.style.width = (screen.width * screen.zoom) + 'px';
+canvas.style.height = (screen.height * screen.zoom) + 'px';
+canvas.style['image-rendering'] = 'pixelated';
+
+const context = canvas.getContext('2d');
+
+function clear() {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, screen.width, screen.height);
+}
+
+function pixel(x, y, color = 'red') {
+    context.fillStyle = color;
+    context.fillRect(x * screen.scale + screen.scale/2 - 2,
+        y * screen.scale +  + screen.scale/2 - 2, 4, 4);
+}
+
+function cell(x, y, color = 'red') {
+    context.fillStyle = color;
+    context.fillRect(x * screen.scale,
+        y * screen.scale, screen.scale, screen.scale);
+}
+
+const FRAMES_EAT_GHOSTS = 40;
+
+let mapStrings =
+    `BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+B                  B                  BB
+B BBBBBBB BBBBBBBB B BBBBBBBB BBBBBBB BB
+B BBBBBBB BBBBBBBB B BBBBBBBB BBBBBBB BB
+BPBBBBBBB BBBBBBBB B BBBBBBBB BBBBBBBPBB
+B BBBBBBB BBBBBBBB B BBBBBBBB BBBBBBB BB
+B                                     BB
+B BBBBBBB BBBBB BBBBBBB BBBBB BBBBBBB BB
+B BBBBBBB BBBBB BBBBBBB BBBBB BBBBBBB BB
+B    BBBB    BB    B    BB    BBBB    BB
+B BB      BB BBBBB B BBBBB BB      BB BB
+B BBBBBBB BB BBBBB B BBBBB BB BBBBBBB BB
+B BB         BBBBB B BBBBB         BB BB
+B    BBBB BB               BB BBBB    BB
+BBBBBBBBB BB BB BBB BBB BB BB BBBBBBBBBB
+BBBBBBBBB    BB B     B BB    BBBBBBBBBB
+BBBBBBBBBBBB BB B     B BB BBBBBBBBBBBBB
+BBBBBBBBBBBB BB B     B BB BBBBBBBBBBBBB
+BBBBBBBBBBBB BB BBB BBB BB BBBBBBBBBBBBB
+BBBBBBBBBBBB               BBBBBBBBBBBBB
+BBBBBBBBBBBB BB BBBBBBB BB BBBBBBBBBBBBB
+BBBBBBBBBBBB BB BBBBBBB BB BBBBBBBBBBBBB
+BBBBBBBBBBBB BB         BB BBBBBBBBBBBBB
+BBBBBBBBBBBB BB BBB BBB BB BBBBBBBBBBBBB
+BBBBBBBBBBBB BB BBB BBB BB BBBBBBBBBBBBB
+B                                     BB
+B BBBBBBB BB BBBBBBBBBBBBB BB BBBBBBB BB
+B BBBBBBB BB BBBBBBBBBBBBB BB BBBBBBB BB
+B      BB BB      BBB      BB BB      BB
+BBBBBB BB BB BBBB BBB BBBB BB BB BBBBBBB
+BBBBBB BB BB BBBB BBB BBBB BB BB BBBBBBB
+B      BB BB BBBB BBB BBBB BB BB      BB
+B BBBB BB BB               BB BB BBBB BB
+B BBBB BB    BBBBBBBBBBBBB    BB BBBB BB
+BPBBBB BBBBB BBBBBBBBBBBBB BBBBB BBBBPBB
+B BBBB            BBB            BBBB BB
+B BBBB BBBBBBBBBB BBB BBBBBBBBBB BBBB BB
+B BBBB BBBBBBBBBB BBB BBBBBBBBBB BBBB BB
+B                                     BB
+BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB`.split('\n');
+
+let mapNumbers = [];
+for (let row = 0; row < mapStrings.length; row++) {
+    let strRow = mapStrings[row];
+    let newRow = [];
+    for (let col = 0; col < strRow.length; col++) {
+        if (strRow[col] == 'B') {
+            newRow.push(1)
+        } else {
+            newRow.push(0)
+        }
+    }
+    mapNumbers.push(newRow);
+}
+
+class Entity {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.color = 'gray';
+    }
+
+    render() {
+        pixel(this.x, this.y, this.color);
+    }
+
+    update() {
+
+    }
+}
+
+const dir_x = [1, 0, -1, 0];
+const dir_y = [0, 1, 0, -1];
+
+class Pacman extends Entity {
+    constructor() {
+        super();
+        this.color = 'yellow';
+        this.dx = 1;
+        this.dy = 0;
+        this.eatGhosts = 0;
+    }
+
+    render() {
+        drawPacman((this.x+0.5) * screen.scale, (this.y+0.5) * screen.scale, this.color, context);
+    }
+
+    update() {
+        if (keyLeft) {
+            this.dx = -1;
+            this.dy = 0;
+        } else if (keyRight) {
+            this.dx = 1;
+            this.dy = 0;
+        } else if (keyUp) {
+            this.dx = 0;
+            this.dy = -1;
+        } else if (keyDown) {
+            this.dx = 0;
+            this.dy = 1;
+        }
+
+        const x_old = this.x;
+        const y_old = this.y;
+        const x_new = x_old + this.dx;
+        const y_new = y_old + this.dy;
+        if (y_new < 0 || y_new >= mapNumbers.length
+            || mapNumbers[y_new][x_new] !== 1) {
+            this.x = x_new;
+            this.y = y_new;
+        }
+
+        if (this.eatGhosts > 0) {
+            this.eatGhosts--;
+        }
+        for (let i=0;i<entities.length;i++) {
+            if (entities[i].x == this.x &&
+                entities[i].y == this.y &&
+                (entities[i] instanceof Fruit
+                || entities[i] instanceof Ghost
+                   && this.eatGhosts
+                )) {
+                if (entities[i].yummy) {
+                    this.eatGhosts = FRAMES_EAT_GHOSTS;
+                }
+                remove(entities[i]);
+            }
+        }
+    }
+}
+
+const dir_love = [0, 0, 0];
+
+class Ghost extends Entity {
+    constructor() {
+        super();
+        this.color = 'red';
+        this.direction = 0;
+    }
+
+    render() {
+        drawGhost(this.x * screen.scale, this.y * screen.scale, this.color, undefined, context);
+    }
+
+    update() {
+        if (pacman.eatGhosts) {
+            this.color = 'aqua';
+        } else {
+            this.color = 'red';
+        }
+
+        dir_love[0] = (this.direction + 3) % 4;
+        dir_love[1] = this.direction;
+        dir_love[2] = (this.direction + 1) % 4;
+        dir_love[3] = (this.direction + 2) % 4;
+
+        if (Math.random() < 0.2) {
+            dir_love[0] = (this.direction + 1) % 4;
+            dir_love[1] = this.direction;
+            dir_love[2] = (this.direction + 3) % 4;
+            dir_love[3] = (this.direction + 2) % 4;
+        }
+
+        for (let i = 0; i < 4; i++) {
+            const dir = dir_love[i];
+            const dx = dir_x[dir];
+            const dy = dir_y[dir];
+
+            const x_old = this.x;
+            const y_old = this.y;
+            const x_new = x_old + dx;
+            const y_new = y_old + dy;
+            if (y_new < 0 || y_new >= mapNumbers.length
+                || mapNumbers[y_new][x_new] !== 1) {
+                this.x = x_new;
+                this.y = y_new;
+                this.direction = dir;
+                break;
+            }
+        }
+
+        for (let i=0;i<entities.length;i++) {
+            if (entities[i].x == this.x &&
+                entities[i].y == this.y &&
+                entities[i] instanceof Pacman &&
+                !entities[i].eatGhosts) {
+                remove(entities[i]);
+            }
+        }
+    }
+}
+
+class Map extends Entity {
+    render() {
+        context.strokeStyle='aqua';
+        for (let i=0;i<paths.length;i++){
+            context.stroke(paths[i]);
+        }
+    }
+}
+
+class Fruit extends Entity {
+    constructor() {
+        super();
+        this.color = 'white';
+        this.yummy = 0;
+    }
+
+    makeyummy() {
+        this.yummy = 1;
+        this.color = 'lime';
+    }
+}
+
+let entities = [];
+
+function add(e) {
+    entities.push(e);
+}
+
+function remove(e) {
+    const ind = entities.indexOf(e);
+    if (ind >= 0) {
+        entities.splice(ind, 1);
+    }
+}
+
+let map = new Map();
+add(map);
+
+let pacman = new Pacman();
+add(pacman);
+pacman.x = 1;
+pacman.y = 2;
+
+const maxW = screen.width / screen.scale;
+const maxH = screen.height / screen.scale;
+
+for (let j = 0; j < maxH; j++) {
+    for (let i = 0; i < maxW; i++) {
+        if (j>=0 &&
+            j<mapNumbers.length &&
+            mapNumbers[j][i]===0) {
+            let fruit = new Fruit();
+            fruit.x = i;
+            fruit.y = j;
+
+            if (mapStrings[j][i] === 'P') {
+                fruit.makeyummy();
+            }
+
+            add(fruit);
+        }
+    }
+}
+
+for (let i = 0; i < 20; i++) {
+    let x = Math.floor(Math.random() * maxW);
+    let y = Math.floor(Math.random() * maxH);
+    if (y < mapNumbers.length && mapNumbers[y][x] === 1) {
+        continue;
+    }
+
+    let ghost = new Ghost();
+    ghost.x = x;
+    ghost.y = y;
+    add(ghost);
+}
+
+function frame() {
+    for (let i = 0; i < entities.length; i++) {
+        entities[i].update();
+    }
+
+    clear();
+    for (let i = 0; i < entities.length; i++) {
+        entities[i].render();
+    }
+}
+
+let keyRight = false;
+let keyUp = false;
+let keyLeft = false;
+let keyDown = false;
+document.addEventListener('keydown', function (event) {
+    switch (event.which) {
+        case 39:
+        case 68:
+            keyRight = true;
+            break;
+        case 38:
+        case 87:
+            keyUp = true;
+            break;
+        case 37:
+        case 65:
+            keyLeft = true;
+            break;
+        case 40:
+        case 83:
+            keyDown = true;
+            break;
+    }
+});
+document.addEventListener('keyup', function (event) {
+    switch (event.which) {
+        case 39:
+        case 68:
+            keyRight = false;
+            break;
+        case 38:
+        case 87:
+            keyUp = false;
+            break;
+        case 37:
+        case 65:
+            keyLeft = false;
+            break;
+        case 40:
+        case 83:
+            keyDown = false;
+            break;
+    }
+});
+
+let paths = [];
+
+let was = [];
+for (let j = 0; j < maxH; j++) {
+    was.push([]);
+}
+
+function addLine(startRow, startCol, startDirection) {
+    const scale = screen.scale;
+
+    const path = new Path2D();
+
+    let row = startRow;
+    let col = startCol;
+    let direction = startDirection;
+    let first = true;
+    do {
+        was[row][col] = true;
+
+        const left = (direction + 3) % 4;
+        const right = (direction + 1) % 4;
+
+        if (first) {
+            path.moveTo((col+0.5) * scale + (dir_x[left] - dir_x[direction]) * scale * 0.4,
+                (row+0.5) * scale + (dir_y[left] - dir_y[direction]) * scale * 0.4);
+            first = false;
+        } else {
+            path.lineTo((col+0.5) * scale + (dir_x[left] - dir_x[direction]) * scale * 0.4,
+                (row+0.5) * scale + (dir_y[left] - dir_y[direction]) * scale * 0.4);
+        }
+
+        dir_love[0] = (direction + 3) % 4;
+        dir_love[1] = direction;
+        dir_love[2] = (direction + 1) % 4;
+        dir_love[3] = (direction + 2) % 4;
+
+        for (let i = 0; i < 4; i++) {
+            const dir = dir_love[i];
+            const dx = dir_x[dir];
+            const dy = dir_y[dir];
+
+            const x_old = col;
+            const y_old = row;
+            const x_new = x_old + dx;
+            const y_new = y_old + dy;
+            if (y_new >= 0 && y_new < mapNumbers.length
+                && mapNumbers[y_new][x_new] === 1) {
+                col = x_new;
+                row = y_new;
+                direction = dir;
+                break;
+            }
+        }
+    } while (row !== startRow
+    || col !== startCol);
+    path.closePath();
+    paths.push(path);
+}
+
+for (let j = 1; j < maxH; j++) {
+    for (let i = 0; i < maxW; i++) {
+        if (j<mapNumbers.length &&
+            mapNumbers[j][i]===1 &&
+            mapNumbers[j-1][i]===0) {
+            if (!was[j][i])
+                addLine(j, i, 0);
+        }
+    }
+}
+
+frame();
+setInterval(frame, 250);
---
+++ mozilla.js
@@ -0,0 +1,55 @@
+function drawPacman(x, y, color, ctx) {
+    ctx.save();
+    ctx.translate(x - 37, y - 37);
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(37,37,13,Math.PI/7,-Math.PI/7,false);
+    ctx.lineTo(31,37);
+    ctx.fill();
+    ctx.restore();
+}
+
+function drawGhost(x, y, color = 'red', bg='black', ctx) {
+    ctx.save();
+    ctx.translate(x - 90, y - 95);
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(83, 116);
+    ctx.lineTo(83, 102);
+    ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+    ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+    ctx.lineTo(111, 116);
+    ctx.lineTo(106.333, 111.333);
+    ctx.lineTo(101.666, 116);
+    ctx.lineTo(97, 111.333);
+    ctx.lineTo(92.333, 116);
+    ctx.lineTo(87.666, 111.333);
+    ctx.lineTo(83, 116);
+    ctx.fill();
+
+    ctx.fillStyle = bg;
+    ctx.beginPath();
+    ctx.moveTo(91, 96);
+    ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+    ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+    ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+    ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+    ctx.moveTo(103, 96);
+    ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+    ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+    ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+    ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+    ctx.fill();
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
+    ctx.fill();
+
+    ctx.restore();
+}
